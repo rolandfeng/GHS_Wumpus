9083fa5899bfa967c67f87a6bce554eb0a7730c3
Allison Gu <GAR_ajgu@seattleschools.org>
2018-04-26 11:16:40 -0700

    commit changes

--- Htw/Htw/components/graphics.cs
+++ Htw/Htw/components/graphics.cs
@@ -11,18 +11,10 @@ namespace wumpus.components
 
         public Graphics()
         {
-            GameControl gameControl = new GameControl();
-            MainGame() mainGame = new MainGame();
-        }
-        public void moveRoom()
-        {
-            if (MainGame.northButton_Click() = )
+
         }
 
 
-        {
-            if ()
 
-        }
     }
 }
--- Htw/Htw/forms/MainGame.cs
+++ Htw/Htw/forms/MainGame.cs
@@ -8,48 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using wumpus.common;
+using wumpus.components;
 
 namespace wumpus.forms
 {
     public partial class MainGame : Form
     {
+        GameControl gameControl; 
         public MainGame()
         {
             InitializeComponent();
-            GameControl gameControl = new GameControl();
+            gameControl = new GameControl();
         }
 
-        private Direction northButton_Click(object sender, EventArgs e)
+        private void northButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.NORTH);
             //return Direction.NORTH;
         }
 
-        private Direction northEastButton_Click(object sender, EventArgs e)
+        private void northEastButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.NORTH_EAST);
             //return Direction.NORTH_EAST;
         }
 
-        private Direction southEastButton_Click(object sender, EventArgs e)
+        private void southEastButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.SOUTH_EAST);
             //return Direction.SOUTH_EAST;
         }
 
-        private Direction southButton_Click(object sender, EventArgs e)
+        private void southButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.SOUTH);
             //return Direction.SOUTH;
         }
 
-        private Direction southWestButton_Click(object sender, EventArgs e)
+        private void southWestButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.SOUTH_WEST);
             //return Direction.SOUTH_WEST;
         }
 
-        private Direction northWestButton_Click(object sender, EventArgs e)
+        private void northWestButton_Click(object sender, EventArgs e)
         {
             gameControl.moveRoom(Direction.NORTH_WEST);
             //return Direction.NORTH_WEST;
