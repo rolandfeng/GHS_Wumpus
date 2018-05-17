2f5fbae8a99d1604854f59fe0a9c4a71753b5e48
Johann Choy <GAR_jjchoy@seattleschools.org>
2018-05-17 11:41:02 -0700

    deleted extraneous methods

--- Htw/Htw/forms/HighScoreForm.cs
+++ Htw/Htw/forms/HighScoreForm.cs
@@ -58,44 +58,10 @@ namespace wumpus.forms
             Date9.Text = "" + highScoresList.ElementAt(9).getHighScore();
         }
 
-        private void HighScore1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label9_Click_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
