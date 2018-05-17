309250d0dd7171fe89c6393dc7a98f4e122ba3c2
Patrick Furman <GAR_pnfurman@seattleschools.org>
2018-05-17 10:58:46 -0700

    started random generation feature, added more testing for cave

--- Htw/HtwTests/components/CaveTest.cs
+++ Htw/HtwTests/components/CaveTest.cs
@@ -14,7 +14,14 @@ namespace HtwTests.components
             Cave cave = new Cave("StandardCave");
             int[] connectedTo1 = cave.getAllConnections(1);
             for (int i = 0; i < connectedTo1.Length; i++)
-                Debug.WriteLine(connectedTo1[i]);
+                Debug.WriteLine(connectedTo1[i]); // Correct nums are 25, 26, 6, 7, 8, 12
+            Debug.WriteLine(cave.getConnectedRoom(19, wumpus.common.Direction.NORTH)); // Should be 0
+            Debug.WriteLine(cave.getConnectedRoom(22, wumpus.common.Direction.SOUTH)); // Should be 0
+            Debug.WriteLine(cave.getConnectedRoom(16, wumpus.common.Direction.NORTH)); // Should be 0
+            Cave altCave = new Cave("AlternativeCave.txt");
+            int[] connectedTo22 = altCave.getAllConnections(22);
+            foreach (int num in connectedTo22)
+                Debug.WriteLine(num); // Correct nums are 16, 17, 15, 0, 28, 27
         }
     }
 }
