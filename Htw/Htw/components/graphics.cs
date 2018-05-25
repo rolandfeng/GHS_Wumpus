6c7edbf101b7ca41543642107ee46d78cff18f2b
Allison Gu <GAR_ajgu@seattleschools.org>
2018-05-25 11:30:27 -0700

    comment out show message

--- Htw/Htw/components/graphics.cs
+++ Htw/Htw/components/graphics.cs
@@ -37,12 +37,13 @@ namespace wumpus.components
             mainGame.UpdateGraphics(currentRoom);
         }
 
-        /* public void Show(String message)
+        /*public void Show(String message)
          {
              System.Windows.Forms.MessageBox.Show(message);
-             break;
+
          }
          */
+         
 
 
     
