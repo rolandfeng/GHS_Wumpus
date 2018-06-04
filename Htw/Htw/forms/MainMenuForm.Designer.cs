2fb38c92838ec6c6ea89834341c48794f91a9296
Allison Gu <allisonjgu420@gmail.com>
2018-06-03 21:56:16 -0700

    display high scores

--- Htw/Htw/forms/MainMenuForm.Designer.cs
+++ Htw/Htw/forms/MainMenuForm.Designer.cs
@@ -83,6 +83,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "High Scores";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainMenuForm
             // 
