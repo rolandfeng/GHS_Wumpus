bc160d72b090a0d1b167bb081297d354c361d2a6
R Feng <rfeng_@hotmail.com>
2018-06-07 20:05:59 -0700

    renamed Form3 to MapForm

--- Htw/Htw/forms/MapForm.cs
+++ Htw/Htw/forms/MapForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace wumpus.forms
 {
-    public partial class Form3 : Form
+    public partial class MapForm : Form
     {
-        public Form3()
+        public MapForm()
         {
             InitializeComponent();
         }
