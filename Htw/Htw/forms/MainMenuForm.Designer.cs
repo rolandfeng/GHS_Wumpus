df262b8fc9ff332e4b44f617bdb906dfaba8da51
JohannBokChoy <jjhchoy2013@gmail.com>
2018-06-08 00:01:53 -0700

    changed sizing

--- Htw/Htw/forms/MainMenuForm.Designer.cs
+++ Htw/Htw/forms/MainMenuForm.Designer.cs
@@ -44,10 +44,10 @@
             // 
             this.startGameButton.BackColor = System.Drawing.Color.MistyRose;
             this.startGameButton.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startGameButton.Location = new System.Drawing.Point(1271, 1000);
-            this.startGameButton.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.startGameButton.Location = new System.Drawing.Point(636, 520);
+            this.startGameButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.startGameButton.Name = "startGameButton";
-            this.startGameButton.Size = new System.Drawing.Size(195, 68);
+            this.startGameButton.Size = new System.Drawing.Size(98, 35);
             this.startGameButton.TabIndex = 0;
             this.startGameButton.Text = "Start Game";
             this.startGameButton.UseVisualStyleBackColor = false;
@@ -57,10 +57,10 @@
             // 
             this.exitButton.BackColor = System.Drawing.Color.MistyRose;
             this.exitButton.Font = new System.Drawing.Font("Showcard Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(1271, 1225);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.exitButton.Location = new System.Drawing.Point(636, 637);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(195, 68);
+            this.exitButton.Size = new System.Drawing.Size(98, 35);
             this.exitButton.TabIndex = 1;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = false;
@@ -72,21 +72,21 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(997, 886);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(518, 436);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(743, 92);
+            this.label1.Size = new System.Drawing.Size(346, 46);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Hunt the Wumpus";
+            this.label1.Text = "Wumpus in Space\r\n";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.MistyRose;
             this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1271, 1075);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(636, 559);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(195, 68);
+            this.button1.Size = new System.Drawing.Size(98, 35);
             this.button1.TabIndex = 3;
             this.button1.Text = "High Scores";
             this.button1.UseVisualStyleBackColor = false;
@@ -96,10 +96,10 @@
             // 
             this.OpenHelp.BackColor = System.Drawing.Color.MistyRose;
             this.OpenHelp.Font = new System.Drawing.Font("Showcard Gothic", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenHelp.Location = new System.Drawing.Point(1271, 1150);
-            this.OpenHelp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OpenHelp.Location = new System.Drawing.Point(636, 598);
+            this.OpenHelp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.OpenHelp.Name = "OpenHelp";
-            this.OpenHelp.Size = new System.Drawing.Size(195, 68);
+            this.OpenHelp.Size = new System.Drawing.Size(98, 35);
             this.OpenHelp.TabIndex = 4;
             this.OpenHelp.Text = "Help";
             this.OpenHelp.UseVisualStyleBackColor = false;
@@ -108,10 +108,10 @@
             // Cave2
             // 
             this.Cave2.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cave2.Location = new System.Drawing.Point(1051, 978);
-            this.Cave2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Cave2.Location = new System.Drawing.Point(526, 509);
+            this.Cave2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Cave2.Name = "Cave2";
-            this.Cave2.Size = new System.Drawing.Size(195, 85);
+            this.Cave2.Size = new System.Drawing.Size(98, 44);
             this.Cave2.TabIndex = 5;
             this.Cave2.Text = "Mystic Galaxy";
             this.Cave2.UseVisualStyleBackColor = true;
@@ -120,10 +120,10 @@
             // Cave1
             // 
             this.Cave1.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cave1.Location = new System.Drawing.Point(833, 978);
-            this.Cave1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Cave1.Location = new System.Drawing.Point(416, 509);
+            this.Cave1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Cave1.Name = "Cave1";
-            this.Cave1.Size = new System.Drawing.Size(195, 85);
+            this.Cave1.Size = new System.Drawing.Size(98, 44);
             this.Cave1.TabIndex = 6;
             this.Cave1.Text = "Glimmer Galaxy";
             this.Cave1.UseVisualStyleBackColor = true;
@@ -132,10 +132,10 @@
             // Cave3
             // 
             this.Cave3.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cave3.Location = new System.Drawing.Point(1271, 978);
-            this.Cave3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Cave3.Location = new System.Drawing.Point(636, 509);
+            this.Cave3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Cave3.Name = "Cave3";
-            this.Cave3.Size = new System.Drawing.Size(195, 85);
+            this.Cave3.Size = new System.Drawing.Size(98, 44);
             this.Cave3.TabIndex = 7;
             this.Cave3.Text = "Twilight Galaxy";
             this.Cave3.UseVisualStyleBackColor = true;
@@ -144,10 +144,10 @@
             // Cave4
             // 
             this.Cave4.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cave4.Location = new System.Drawing.Point(1491, 978);
-            this.Cave4.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Cave4.Location = new System.Drawing.Point(746, 509);
+            this.Cave4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Cave4.Name = "Cave4";
-            this.Cave4.Size = new System.Drawing.Size(195, 85);
+            this.Cave4.Size = new System.Drawing.Size(98, 44);
             this.Cave4.TabIndex = 8;
             this.Cave4.Text = "Celestial Galaxy";
             this.Cave4.UseVisualStyleBackColor = true;
@@ -156,10 +156,10 @@
             // Cave5
             // 
             this.Cave5.Font = new System.Drawing.Font("Showcard Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cave5.Location = new System.Drawing.Point(1712, 978);
-            this.Cave5.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Cave5.Location = new System.Drawing.Point(856, 509);
+            this.Cave5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Cave5.Name = "Cave5";
-            this.Cave5.Size = new System.Drawing.Size(195, 85);
+            this.Cave5.Size = new System.Drawing.Size(98, 44);
             this.Cave5.TabIndex = 9;
             this.Cave5.Text = "sapphire galaxy";
             this.Cave5.UseVisualStyleBackColor = true;
@@ -167,12 +167,12 @@
             // 
             // MainMenuForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CadetBlue;
             this.BackgroundImage = global::wumpus.Properties.Resources.startBackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(2248, 1415);
+            this.ClientSize = new System.Drawing.Size(1354, 1061);
             this.Controls.Add(this.Cave5);
             this.Controls.Add(this.Cave4);
             this.Controls.Add(this.Cave3);
@@ -183,7 +183,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.startGameButton);
-            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximumSize = new System.Drawing.Size(1370, 1100);
+            this.MinimumSize = new System.Drawing.Size(1370, 1100);
             this.Name = "MainMenuForm";
             this.Text = "Wumpus";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
