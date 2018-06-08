d1d4aadc3060b12fd00ac96636deb2c618894f6a
R Feng <rfeng_@hotmail.com>
2018-06-07 22:23:20 -0700

    .

--- Htw/Htw/forms/MapForm.Designer.cs
+++ Htw/Htw/forms/MapForm.Designer.cs
@@ -64,279 +64,310 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 74);
+            this.label1.Location = new System.Drawing.Point(138, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(112, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Pandora-1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 118);
+            this.label2.Location = new System.Drawing.Point(296, 227);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(95, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Aurora-2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(220, 74);
+            this.label3.Location = new System.Drawing.Point(440, 142);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(97, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Avalon-3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(300, 115);
+            this.label4.Location = new System.Drawing.Point(600, 221);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.Size = new System.Drawing.Size(73, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Zion-4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(369, 70);
+            this.label5.Location = new System.Drawing.Point(738, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.Size = new System.Drawing.Size(81, 25);
             this.label5.TabIndex = 4;
             this.label5.Text = "Nova-5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(435, 115);
+            this.label6.Location = new System.Drawing.Point(870, 221);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.Size = new System.Drawing.Size(123, 25);
             this.label6.TabIndex = 5;
             this.label6.Text = "Spartania-6";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(75, 164);
+            this.label7.Location = new System.Drawing.Point(150, 315);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.Size = new System.Drawing.Size(103, 25);
             this.label7.TabIndex = 6;
             this.label7.Text = "Artemis-7";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(147, 210);
+            this.label8.Location = new System.Drawing.Point(294, 404);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.Size = new System.Drawing.Size(110, 25);
             this.label8.TabIndex = 7;
             this.label8.Text = "Genesis-8";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(220, 164);
+            this.label9.Location = new System.Drawing.Point(440, 315);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.Size = new System.Drawing.Size(93, 25);
             this.label9.TabIndex = 8;
             this.label9.Text = "Xenon-9";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(297, 205);
+            this.label10.Location = new System.Drawing.Point(594, 394);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.Size = new System.Drawing.Size(95, 25);
             this.label10.TabIndex = 9;
             this.label10.Text = "Orion-10";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(367, 160);
+            this.label11.Location = new System.Drawing.Point(734, 308);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(51, 13);
+            this.label11.Size = new System.Drawing.Size(103, 25);
             this.label11.TabIndex = 10;
             this.label11.Text = "Apollo-11";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(435, 203);
+            this.label12.Location = new System.Drawing.Point(870, 390);
+            this.label12.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(61, 13);
+            this.label12.Size = new System.Drawing.Size(123, 25);
             this.label12.TabIndex = 11;
             this.label12.Text = "Stardust-12";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(70, 253);
+            this.label13.Location = new System.Drawing.Point(140, 487);
+            this.label13.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(64, 13);
+            this.label13.Size = new System.Drawing.Size(129, 25);
             this.label13.TabIndex = 12;
             this.label13.Text = "Hyperion-13";
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(150, 297);
+            this.label14.Location = new System.Drawing.Point(300, 571);
+            this.label14.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(56, 13);
+            this.label14.Size = new System.Drawing.Size(113, 25);
             this.label14.TabIndex = 13;
             this.label14.Text = "Divinity-14";
             // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(223, 253);
+            this.label15.Location = new System.Drawing.Point(446, 487);
+            this.label15.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(50, 13);
+            this.label15.Size = new System.Drawing.Size(102, 25);
             this.label15.TabIndex = 14;
             this.label15.Text = "Trinity-15";
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(298, 295);
+            this.label16.Location = new System.Drawing.Point(596, 567);
+            this.label16.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(47, 13);
+            this.label16.Size = new System.Drawing.Size(98, 25);
             this.label16.TabIndex = 15;
             this.label16.Text = "Osiris-16";
             // 
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(366, 248);
+            this.label17.Location = new System.Drawing.Point(732, 477);
+            this.label17.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(57, 13);
+            this.label17.Size = new System.Drawing.Size(118, 25);
             this.label17.TabIndex = 16;
             this.label17.Text = "Elysium-17";
             // 
             // label18
             // 
             this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(439, 287);
+            this.label18.Location = new System.Drawing.Point(878, 552);
+            this.label18.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(59, 13);
+            this.label18.Size = new System.Drawing.Size(117, 25);
             this.label18.TabIndex = 17;
             this.label18.Text = "Nirvana-18";
             // 
             // label19
             // 
             this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(85, 340);
+            this.label19.Location = new System.Drawing.Point(170, 654);
+            this.label19.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(42, 13);
+            this.label19.Size = new System.Drawing.Size(85, 25);
             this.label19.TabIndex = 18;
             this.label19.Text = "Styx-19";
             // 
             // label20
             // 
             this.label20.AutoSize = true;
-            this.label20.Location = new System.Drawing.Point(155, 385);
+            this.label20.Location = new System.Drawing.Point(310, 740);
+            this.label20.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(52, 13);
+            this.label20.Size = new System.Drawing.Size(105, 25);
             this.label20.TabIndex = 19;
             this.label20.Text = "Vortex-20";
             // 
             // label21
             // 
             this.label21.AutoSize = true;
-            this.label21.Location = new System.Drawing.Point(222, 340);
+            this.label21.Location = new System.Drawing.Point(444, 654);
+            this.label21.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label21.Name = "label21";
-            this.label21.Size = new System.Drawing.Size(60, 13);
+            this.label21.Size = new System.Drawing.Size(121, 25);
             this.label21.TabIndex = 20;
             this.label21.Text = "Oblivion-21";
             // 
             // label22
             // 
             this.label22.AutoSize = true;
-            this.label22.Location = new System.Drawing.Point(300, 380);
+            this.label22.Location = new System.Drawing.Point(600, 731);
+            this.label22.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label22.Name = "label22";
-            this.label22.Size = new System.Drawing.Size(50, 13);
+            this.label22.Size = new System.Drawing.Size(100, 25);
             this.label22.TabIndex = 21;
             this.label22.Text = "Argon-22";
             // 
             // label23
             // 
             this.label23.AutoSize = true;
-            this.label23.Location = new System.Drawing.Point(365, 335);
+            this.label23.Location = new System.Drawing.Point(730, 644);
+            this.label23.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label23.Name = "label23";
-            this.label23.Size = new System.Drawing.Size(57, 13);
+            this.label23.Size = new System.Drawing.Size(115, 25);
             this.label23.TabIndex = 22;
             this.label23.Text = "Exodus-23";
             // 
             // label24
             // 
             this.label24.AutoSize = true;
-            this.label24.Location = new System.Drawing.Point(439, 377);
+            this.label24.Location = new System.Drawing.Point(878, 725);
+            this.label24.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label24.Name = "label24";
-            this.label24.Size = new System.Drawing.Size(61, 13);
+            this.label24.Size = new System.Drawing.Size(126, 25);
             this.label24.TabIndex = 23;
             this.label24.Text = "Celestial-24";
             // 
             // label25
             // 
             this.label25.AutoSize = true;
-            this.label25.Location = new System.Drawing.Point(77, 430);
+            this.label25.Location = new System.Drawing.Point(154, 827);
+            this.label25.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label25.Name = "label25";
-            this.label25.Size = new System.Drawing.Size(60, 13);
+            this.label25.Size = new System.Drawing.Size(121, 25);
             this.label25.TabIndex = 24;
             this.label25.Text = "Equinox-25";
             // 
             // label26
             // 
             this.label26.AutoSize = true;
-            this.label26.Location = new System.Drawing.Point(155, 475);
+            this.label26.Location = new System.Drawing.Point(310, 913);
+            this.label26.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label26.Name = "label26";
-            this.label26.Size = new System.Drawing.Size(56, 13);
+            this.label26.Size = new System.Drawing.Size(111, 25);
             this.label26.TabIndex = 25;
             this.label26.Text = "Nebula-26";
             // 
             // label27
             // 
             this.label27.AutoSize = true;
-            this.label27.Location = new System.Drawing.Point(215, 428);
+            this.label27.Location = new System.Drawing.Point(430, 823);
+            this.label27.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label27.Name = "label27";
-            this.label27.Size = new System.Drawing.Size(76, 13);
+            this.label27.Size = new System.Drawing.Size(153, 25);
             this.label27.TabIndex = 26;
             this.label27.Text = "Andromeda-27";
             // 
             // label28
             // 
             this.label28.AutoSize = true;
-            this.label28.Location = new System.Drawing.Point(295, 470);
+            this.label28.Location = new System.Drawing.Point(590, 904);
+            this.label28.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label28.Name = "label28";
-            this.label28.Size = new System.Drawing.Size(61, 13);
+            this.label28.Size = new System.Drawing.Size(121, 25);
             this.label28.TabIndex = 27;
             this.label28.Text = "Galactic-28";
             // 
             // label29
             // 
             this.label29.AutoSize = true;
-            this.label29.Location = new System.Drawing.Point(365, 425);
+            this.label29.Location = new System.Drawing.Point(730, 817);
+            this.label29.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label29.Name = "label29";
-            this.label29.Size = new System.Drawing.Size(62, 13);
+            this.label29.Size = new System.Drawing.Size(125, 25);
             this.label29.TabIndex = 28;
             this.label29.Text = "Titanium-29";
             // 
             // label30
             // 
             this.label30.AutoSize = true;
-            this.label30.Location = new System.Drawing.Point(435, 466);
+            this.label30.Location = new System.Drawing.Point(870, 896);
+            this.label30.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label30.Name = "label30";
-            this.label30.Size = new System.Drawing.Size(71, 13);
+            this.label30.Size = new System.Drawing.Size(145, 25);
             this.label30.TabIndex = 29;
             this.label30.Text = "Sagittarius-30";
             // 
-            // Form3
+            // MapForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(579, 567);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1158, 1090);
             this.Controls.Add(this.label30);
             this.Controls.Add(this.label29);
             this.Controls.Add(this.label28);
@@ -367,7 +398,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form3";
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MapForm";
             this.Text = "Form3";
             this.ResumeLayout(false);
             this.PerformLayout();
